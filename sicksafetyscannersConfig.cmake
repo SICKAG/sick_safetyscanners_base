6a35eb83c1bc73c8ffa99476ce88db4c756ca359
Lennart Puck <puck@fzi.de>
2019-08-14 17:03:57 +0200

    added config cmake

---
+++ sicksafetyscannersConfig.cmake
@@ -0,0 +1,6 @@
+include(CMakeFindDependencyMacro)
+#find_dependency(Threads)
+# Note that find_dependency does not support COMPONENTS or MODULE until 3.8.0
+find_package(Boost COMPONENTS system thread REQUIRED)
+include("${CMAKE_CURRENT_LIST_DIR}/sicksafetyscannersTargets.cmake")
+
