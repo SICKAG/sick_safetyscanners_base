04b0580e69116a3f45ec35fccee95504264bd90c
Lennart Puck <puck@fzi.de>
2020-12-09 19:37:08 +0100

    fixed bug with wrong angle configuration
    missing multiplication required from cola2 procoll included

--- src/cola2/ChangeCommSettingsCommand.cpp
+++ src/cola2/ChangeCommSettingsCommand.cpp
@@ -122,13 +122,15 @@ void ChangeCommSettingsCommand::writeFrequencyToDataPtr(
 void ChangeCommSettingsCommand::writeStartAngleToDataPtr(
   std::vector<uint8_t>::iterator data_ptr) const
 {
-  read_write_helper::writeUint32LittleEndian(data_ptr + 16, m_settings.start_angle);
+  // multiply by constant which is required by the sensor
+  read_write_helper::writeUint32LittleEndian(data_ptr + 16, m_settings.start_angle * 4194304.0);
 }
 
 void ChangeCommSettingsCommand::writeEndAngleToDataPtr(
   std::vector<uint8_t>::iterator data_ptr) const
 {
-  read_write_helper::writeUint32LittleEndian(data_ptr + 20, m_settings.end_angle);
+  // multiply by constant which is required by the sensor
+  read_write_helper::writeUint32LittleEndian(data_ptr + 20, m_settings.end_angle * 4194304.0);
 }
 
 void ChangeCommSettingsCommand::writeFeaturesToDataPtr(
